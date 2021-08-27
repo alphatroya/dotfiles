213fe4d233094e59f50f8f41059c58a81b0d1907
Alexey Korolev <alphatroya@gmail.com>
2021-08-27 14:23:32 +0300

    Add mapping for search history cycle moving

--- nvim/lua/config/telescope/init.lua
+++ nvim/lua/config/telescope/init.lua
@@ -4,3 +4,20 @@ vim.api.nvim_set_keymap('n', '<leader>fg', [[<cmd>lua require('telescope.builtin
 vim.api.nvim_set_keymap('n', '<leader>fb', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>f?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], { noremap = true, silent = true })
 
+-- Mapping for search history moving
+local status_ok, actions = pcall(require, "telescope.actions")
+if not status_ok then
+    return
+end
+
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-n>"] = actions.cycle_history_next,
+        ["<C-p>"] = actions.cycle_history_prev,
+      },
+    },
+  }
+}
+
