c78f3fa7f2e7758920e526a6b6815a961475bf09
Alexey Korolev <alphatroya@gmail.com>
2022-05-20 09:48:39 +0300

    Remove forgit zsh plugin

--- nvim/lua/config/cmp/init.lua
+++ nvim/lua/config/cmp/init.lua
@@ -33,7 +33,6 @@ cmp.setup({
                 fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
             end
         end, {"i","s","c",}),
-        ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
         ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp_types.SelectBehavior.Insert }),
         ['<down>'] = cmp.mapping.select_next_item({ behavior = cmp_types.SelectBehavior.Insert }),
         ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp_types.SelectBehavior.Insert }),
