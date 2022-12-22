c1b514cc303542a644935d7be11c7bcaaddbb3ee
Alexey Korolev <alphatroya@gmail.com>
2022-12-22 12:02:36 +0100

    Add <leader>/ keymap

--- nvim/lua/config/mapping/init.lua
+++ nvim/lua/config/mapping/init.lua
@@ -9,14 +9,20 @@ end, { silent = true, desc = '[S]earch [N]eighbors' })
 vim.keymap.set('n', '<leader>sc', ':Telescope git_status<CR>', { silent = true, desc = '[S]earch [C]hanged' })
 vim.keymap.set('n', '<leader>sg', ':Telescope live_grep<CR>', { silent = true, desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sw', ':Telescope grep_string<CR>', { silent = true, desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>s?', ':Telescope oldfiles<CR>', { silent = true, desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader>s/', ':Telescope resume<CR>', { silent = true })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostic' })
-vim.keymap.set('n', 'z=', require('telescope.builtin').spell_suggest)
 vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
 vim.keymap.set('n', '<leader>ss', require('telescope.builtin').lsp_document_symbols,
     { silent = true, desc = '[S]earch Document [S]ymbols' })
 vim.keymap.set('n', '<leader>st', ':TodoTelescope<CR>', { silent = true, desc = '[S]earch [T]ODO marks' })
+vim.keymap.set('n', '<leader>/', function()
+    require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = false,
+    })
+end, { desc = '[/] Fuzzily search in current buffer]' })
+vim.keymap.set('n', '<leader>?', ':Telescope oldfiles<CR>', { silent = true, desc = '[?] Find recently opened files' })
+vim.keymap.set('n', 'z=', require('telescope.builtin').spell_suggest)
 
 -- Diagnostic
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
@@ -37,6 +43,7 @@ vim.keymap.set('n', "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
 vim.keymap.set('n', "<leader>rn", "<cmd>Lspsaga rename<CR>", { silent = true })
 vim.api.nvim_set_keymap("n", "<leader>gr", "<cmd>Lspsaga lsp_finder<CR>",
     { silent = true, desc = '[G]oto [R]eferences' })
+
 
 -- Remaps for the refactoring
 require("telescope").load_extension("refactoring")
