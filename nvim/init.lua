5a00c16fc93877216da1097eeac25b97d9329c6f
Alexey Korolev <alphatroya@gmail.com>
2023-01-31 12:58:30 +0100

    Rename todo-comments

--- nvim/init.lua
+++ nvim/init.lua
@@ -236,13 +236,12 @@ require("lazy").setup({
     -- todo hightlighting
     {
         'folke/todo-comments.nvim',
+        name = "todo-comments",
         dependencies = 'nvim-lua/plenary.nvim',
         keys = {
             { "<leader>st", ':TodoTelescope<CR>', desc = '[S]earch [T]ODO marks' },
         },
-        config = function()
-            require('todo-comments').setup()
-        end,
+        config = true,
     },
 
     {
