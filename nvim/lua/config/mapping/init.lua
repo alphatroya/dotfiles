c0204898e75b982c09f331bf9b16cc4c548df26f
Alexey Korolev <alphatroya@gmail.com>
2023-01-31 15:49:38 +0100

    Add keymap for copy/paste from clipboard

--- nvim/lua/config/mapping/init.lua
+++ nvim/lua/config/mapping/init.lua
@@ -8,6 +8,12 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 vim.keymap.set('n', ']b', ':bnext<CR>')
 vim.keymap.set('n', '[b', ':bprev<CR>')
 
+-- Copy/paste from clipboard
+vim.keymap.set({ 'n', 'x' }, 'gy', '"+y', { desc = 'Copy to system clipboard' })
+vim.keymap.set('n', 'gp', '"+p', { desc = 'Paste from system clipboard' })
+-- Paste in Visual with `P` to not copy selected text (`:h v_P`)
+vim.keymap.set('x', 'gp', '"+P', { desc = 'Paste from system clipboard' })
+
 -- LSP
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { silent = true, desc = '[G]oto [D]efinition' })
 vim.keymap.set('n', 'gD', vim.lsp.buf.type_definition, { silent = true, desc = '[G]oto type [D]efinition' })
