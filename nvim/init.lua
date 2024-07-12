eb61cc57a6e88abdc2ade54843bd60a5ac6ce5f9
Alexey Korolev <alphatroya@gmail.com>
2024-07-12 11:10:41 +0300

    Remove folke/which-key.nvim plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -339,28 +339,6 @@ require("lazy").setup({
         end,
     },
 
-    -- WhichKey is a lua plugin that displays a popup with possible keybindings of the command you started typing.
-    {
-        "folke/which-key.nvim",
-        config = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-
-            local lmu = require('langmapper.utils')
-            local view = require('which-key.view')
-            local execute = view.execute
-
-            -- wrap `execute()` and translate sequence back
-            view.execute = function(prefix_i, mode, buf)
-                -- Translate back to English characters
-                prefix_i = lmu.translate_keycode(prefix_i, 'default', 'ru')
-                execute(prefix_i, mode, buf)
-            end
-
-            require("which-key").setup { plugins = { spelling = { enabled = true, }, }, }
-        end,
-    },
-
     -- A plugin that makes Neovim more friendly to non-English input methods 🤝
     {
         'Wansmer/langmapper.nvim',
